c82f4978d9533e7efdb68c7217392767fa3f0338
“nameisjar” <nthia4@gmail.com>
2023-06-30 17:02:36 +0700

    tes

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,17 +81,18 @@ function resetForm(...inputs) {
 // }
 
 "use strict";
-// Inspired By
-// https://codepen.io/abeatrize/pen/LJqYey
-var _a, _b;
-// Bongo Cat originally created by @StrayRogue and @DitzyFlama
+
 const ID = "bongo-cat";
 const s = (selector) => `#${ID} ${selector}`;
 const notes = document.querySelectorAll(".note");
 for (let note of notes) {
-  (_a = note === null || note === void 0 ? void 0 : note.parentElement) === null || _a === void 0 ? void 0 : _a.appendChild(note.cloneNode(true));
-  (_b = note === null || note === void 0 ? void 0 : note.parentElement) === null || _b === void 0 ? void 0 : _b.appendChild(note.cloneNode(true));
+  const parentElement = note?.parentElement;
+  if (parentElement) {
+    parentElement.appendChild(note.cloneNode(true));
+    parentElement.appendChild(note.cloneNode(true));
+  }
 }
+
 const music = { note: s(".music .note") };
 const cat = {
   pawRight: {
@@ -103,69 +104,68 @@ const cat = {
     down: s(".paw-left .down"),
   },
 };
-const style = getComputedStyle(document.documentElement);
-const green = style.getPropertyValue("--green");
-const pink = style.getPropertyValue("--pink");
-const blue = style.getPropertyValue("--blue");
-const orange = style.getPropertyValue("--orange");
-const cyan = style.getPropertyValue("--cyan");
-gsap.set(music.note, { scale: 0, autoAlpha: 1 });
-const animatePawState = (selector) => gsap.fromTo(selector, { autoAlpha: 0 }, {
-  autoAlpha: 1,
-  duration: 0.01,
-  repeatDelay: 0.19,
-  yoyo: true,
-  repeat: -1,
+
+const colorizer = () => anime.random(["#00ff00", "#ff00ff", "#0000ff", "#ff6600", "#00ffff", "#a3a4ec", "#67b5c0", "#fd7c6e"]);
+const rotator = () => anime.random(-50, 50);
+const dir = (amt) => `${anime.random(["-", "+"])}=${amt}`;
+
+const animatePawState = (selector) => {
+  anime({
+    targets: selector,
+    opacity: [0, 1],
+    duration: 10,
+    delay: 190,
+    direction: 'alternate',
+    loop: true,
+  });
+};
+
+anime({
+  targets: ".terminal-code line",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: "linear",
+  duration: 100,
+  delay: anime.stagger(100, { from: "random" }),
+  direction: "alternate",
+  loop: true,
 });
-const tl = gsap.timeline();
-tl.add(animatePawState(cat.pawLeft.up), "start")
-  .add(animatePawState(cat.pawRight.down), "start")
-  .add(animatePawState(cat.pawLeft.down), "start+=0.19")
-  .add(animatePawState(cat.pawRight.up), "start+=0.19")
-  .timeScale(1.6);
-gsap.from(".terminal-code line", {
-  drawSVG: "0%",
-  duration: 0.1,
-  stagger: 0.1,
-  ease: "none",
-  repeat: -1,
-});
-// typing for pipe function doesn't seem to be working for usage when partially applied?
-const noteElFn = gsap.utils.pipe(gsap.utils.toArray, gsap.utils.shuffle);
+
+const animateNotes = (els) => {
+  els.forEach((el) => {
+    anime.set(el, {
+      stroke: colorizer(),
+      rotate: rotator(),
+      translateX: anime.random(-25, 25),
+    });
+  });
+  anime({
+    targets: els,
+    opacity: [1, 0],
+    scale: [0, 1],
+    duration: 2000,
+    easing: "linear",
+    delay: anime.stagger(500, { from: "random" }),
+    rotate: dir(anime.random(20, 30)),
+    translateX: dir(anime.random(40, 60)),
+    translateY: anime.random(-200, -220),
+    complete: () => animateNotes(els),
+  });
+};
+
+animatePawState(cat.pawLeft.up);
+animatePawState(cat.pawRight.down);
+animatePawState(cat.pawLeft.down);
+animatePawState(cat.pawRight.up);
+
+const noteElFn = anime.random;
 const noteEls = noteElFn(music.note);
 const numNotes = noteEls.length / 3;
 const notesG1 = noteEls.splice(0, numNotes);
 const notesG2 = noteEls.splice(0, numNotes);
 const notesG3 = noteEls;
-const colorizer = gsap.utils.random([green, pink, blue, orange, cyan, "#a3a4ec", "#67b5c0", "#fd7c6e"], true);
-const rotator = gsap.utils.random(-50, 50, 1, true);
-const dir = (amt) => `${gsap.utils.random(["-", "+"])}=${amt}`;
-const animateNotes = (els) => {
-  els.forEach((el) => {
-    gsap.set(el, {
-      stroke: colorizer(),
-      rotation: rotator(),
-      x: gsap.utils.random(-25, 25, 1),
-    });
-  });
-  return gsap.fromTo(els, {
-    autoAlpha: 1,
-    y: 0,
-    scale: 0,
-  }, {
-    duration: 2,
-    autoAlpha: 0,
-    scale: 1,
-    ease: "none",
-    stagger: {
-      from: "random",
-      each: 0.5,
-    },
-    rotation: dir(gsap.utils.random(20, 30, 1)),
-    x: dir(gsap.utils.random(40, 60, 1)),
-    y: gsap.utils.random(-200, -220, 1),
-    onComplete: () => animateNotes(els),
-  });
-};
-tl.add(animateNotes(notesG1)).add(animateNotes(notesG2), ">0.05").add(animateNotes(notesG3), ">0.25");
+
+animateNotes(notesG1);
+setTimeout(() => animateNotes(notesG2), 50);
+setTimeout(() => animateNotes(notesG3), 250);
+
 
